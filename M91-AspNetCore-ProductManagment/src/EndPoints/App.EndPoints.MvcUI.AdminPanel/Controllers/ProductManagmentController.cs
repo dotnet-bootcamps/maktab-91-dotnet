2e9c3a61c98bbb65ad857c46e684355e57624e80
msavarian <sav68.net@gmail.com>
2023-03-03 10:52:55 +0330

    add delete and add actions

--- M91-AspNetCore-ProductManagment/src/EndPoints/App.EndPoints.MvcUI.AdminPanel/Controllers/ProductManagmentController.cs
+++ M91-AspNetCore-ProductManagment/src/EndPoints/App.EndPoints.MvcUI.AdminPanel/Controllers/ProductManagmentController.cs
@@ -13,6 +13,25 @@ namespace App.EndPoints.MvcUI.AdminPanel.Controllers
             return View("Index", _productList);
         }
 
+        public IActionResult Add()
+        {
+            return View(new Product());
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product newProductModel)
+        {
+            var lastProductRecord = _productList.OrderByDescending(o => o.Id).FirstOrDefault();
+
+            if (lastProductRecord is null)
+                newProductModel.Id = 1;
+            else
+                newProductModel.Id = lastProductRecord.Id + 1;
+            
+            _productList.Add(newProductModel);
+            return RedirectToAction("Index");
+        }
+
         public IActionResult ShowDetails(int id)
         {
             var selectedProduct = _productList.Find(x => x.Id == id);
@@ -45,6 +64,32 @@ namespace App.EndPoints.MvcUI.AdminPanel.Controllers
             selectedProduct.Title = model.Title;
             selectedProduct.Price = model.Price;
             selectedProduct.Qty = model.Qty;
+
+            return RedirectToAction("Index");
+            // return View("Index", _productList);
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var selectedProduct = _productList.Find(x => x.Id == id);
+
+            if (selectedProduct is null)
+                throw new Exception($"There is no product with id {id}");
+
+            return View(selectedProduct);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Product model)
+        {
+            var selectedProduct = _productList.Find(x => x.Id == model.Id);
+
+            if (selectedProduct is null)
+                throw new Exception($"There is no product with id {model.Id}");
+
+            _productList.Remove(selectedProduct);
 
             return RedirectToAction("Index");
             // return View("Index", _productList);
